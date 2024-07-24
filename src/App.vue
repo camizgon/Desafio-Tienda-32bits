9c0dba51bbe9e04a35e8d5f6efa64f70cae33430
camizgon <camizgon22@hotmail.com>
2024-07-24 14:50:58 -0400

    Tienda 32 Bits

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,93 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <h1>Tienda 32 Bits</h1>
+    <h2>Lista de juegos</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Código</th>
+          <th>Nombre</th>
+          <th>Stock</th>
+          <th>Precio</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(juego, index) in juegos" :key="juego.codigo">
+          <td>{{ juego.codigo }}</td>
+          <td>{{ juego.nombre }}</td>
+          <td>{{ juego.stock }}</td>
+          <td>${{ juego.precio }}</td>
+          <td>
+          <!--Los métodos decrementStock e incrementStock son llamados con el index del juego actual. Esto asegura que cuando se hace clic en los botones, la acción sabe exactamente qué juego debe actualizarse.-->
+            <button @click="decrementStock(index)" :disabled="juego.stock <= 0">-</button>
+            <button @click="incrementStock(index)">+</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  name: 'App',
+  computed: {
+    ...mapState(['juegos'])
+  },
+  methods: {
+    ...mapActions(["incrementStock", "decrementStock"]),
+  }
+};
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   text-align: center;
-  color: #2c3e50;
+  margin-top: 60px;
 }
 
-nav {
-  padding: 30px;
+table {
+  width: 80%;
+  margin: 0 auto;
+  border-collapse: collapse;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+th, td {
+  border: 1px solid #ddd;
+  padding: 8px;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+th {
+  background-color: #f2f2f2;
+  text-align: center;
+}
+
+td {
+  text-align: center;
+}
+
+button {
+  margin: 0 5px;
+  padding: 5px 10px;
+  border: 0.5px solid #bdbdbd;
+  background-color: #3d98e8;
+  color: #fff;
+  cursor: pointer;
+}
+button:hover{
+  background-color: #10518a;
+}
+
+button:disabled {
+  background-color: #ccc;
+  cursor: not-allowed;
 }
 </style>
+
+
+
+
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,33 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import juegos from "../store/juegos.json";
 
 export default createStore({
   state: {
+    juegos: juegos
   },
-  getters: {
-  },
+
+  //incrementStock(state, index) y decrementStock(state, index): Las mutaciones en Vuex reciben el estado actual (state) y el índice del juego a modificar. Esto permite a las mutaciones actualizar el stock del juego correcto en la lista juegos.
   mutations: {
+    incrementStock(state, index) {
+      state.juegos[index].stock++;
+    },
+    decrementStock(state, index) {
+      if (state.juegos[index].stock > 0) {
+        state.juegos[index].stock--;
+      }
+    },
   },
+
+  //incrementStock(context, index) y decrementStock(context, index): Las acciones también reciben el contexto de Vuex (context) y el índice del juego a modificar. Las acciones luego comiten las mutaciones correspondientes, pasando el índice recibido.
   actions: {
+    incrementStock(context, index) {
+      context.commit("incrementStock", index);
+    },
+    decrementStock(context, index) {
+      context.commit("decrementStock", index);
+    },
   },
-  modules: {
-  }
-})
+});
+
+
+//El uso de index asegura que cada operación de incremento y decremento del stock se aplique al juego correcto en la lista.
